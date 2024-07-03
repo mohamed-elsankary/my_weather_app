461d56ae85e3920adee41d4df885e44956a5c49a
Mohamed2002-G3 <mohamedelsankary14@gmail.com>
2024-07-03 19:49:51 +0300

    create api service

--- lib/core/utils/widget/api_service.dart
+++ lib/core/utils/widget/api_service.dart
@@ -1,1 +1,13 @@
+import 'package:dio/dio.dart';
 
+class ApiService {
+  final _baseUrl =
+      'http://api.weatherapi.com/v1/marine.json?key=933b8e9ec7d54634b16100839241903&q=cairo&days=3';
+  final Dio _dio;
+  ApiService(this._dio);
+
+  Future<Map<String, dynamic>> getWeather() async {
+    final response = await _dio.get(_baseUrl);
+    return response.data;
+  }
+}
